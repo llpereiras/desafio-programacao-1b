4a030deeb130f01514897109b6c4f42e47a58e68
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-04 19:01:17 -0200

    Adicionando configurações de tradução

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module DesafioProgramacao1b
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :"ptBR"
+    config.i18n.fallbacks = false
+    
   end
 end
