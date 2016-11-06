52acc4cf770b5e9528852a0dc9ef0ab53215a5dd
Leandro Pereira <llpereiras@gmail.com>
2016-11-06 13:57:49 -0200

    Retirando metodo que não serao mais utilizados

--- app/controllers/vendas_controller.rb
+++ app/controllers/vendas_controller.rb
@@ -1,92 +1,9 @@
 class VendasController < ApplicationController
   before_action :set_venda, only: [:show, :edit, :update, :destroy]
 
-  def upload
-    message = nil
-    file = 'public/uploads/dados.txt'
-    thread = Thread.new {
-      begin
-        ::Importacao::Interprete.processar_arquivo(file)
-      rescue Exception => e
-        message = "#{DateTime.now.to_s} - EXCEPTION - #{e.message} - #{e.backtrace.each{|l| puts l }} \n\n"
-      ensure
-        File.open("#{Rails.root}/log/importacao.log", "a+") { |f| f << message } if message
-        Thread::exit()
-      end
-    }
-    respond_to do |format|
-      format.html { redirect_to "/", notice: 'Upload efetuado com successo.' }
-    end
-  end
-
   # GET /vendas
   # GET /vendas.json
   def index
     @vendas = Venda.all
   end
-
-  # GET /vendas/1
-  # GET /vendas/1.json
-  def show
-  end
-
-  # GET /vendas/new
-  def new
-    @venda = Venda.new
-  end
-
-  # GET /vendas/1/edit
-  def edit
-  end
-
-  # POST /vendas
-  # POST /vendas.json
-  def create
-    @venda = Venda.new(venda_params)
-
-    respond_to do |format|
-      if @venda.save
-        format.html { redirect_to @venda, notice: 'Venda was successfully created.' }
-        format.json { render :show, status: :created, location: @venda }
-      else
-        format.html { render :new }
-        format.json { render json: @venda.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /vendas/1
-  # PATCH/PUT /vendas/1.json
-  def update
-    respond_to do |format|
-      if @venda.update(venda_params)
-        format.html { redirect_to @venda, notice: 'Venda was successfully updated.' }
-        format.json { render :show, status: :ok, location: @venda }
-      else
-        format.html { render :edit }
-        format.json { render json: @venda.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /vendas/1
-  # DELETE /vendas/1.json
-  def destroy
-    @venda.destroy
-    respond_to do |format|
-      format.html { redirect_to vendas_url, notice: 'Venda was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_venda
-      @venda = Venda.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def venda_params
-      params.require(:venda).permit(:cliente_id, :fornecedor_id)
-    end
 end
