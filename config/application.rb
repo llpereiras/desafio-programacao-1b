490350b5dd7c578f40391d0b9c31d54878f3ae78
Leandro Pereira <llpereiras@gmail.com>
2016-11-06 12:25:58 -0200

    Adicionando path para auto_load

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module DesafioProgramacao1b
     config.time_zone = 'Brasilia'
     config.active_record.default_timezone = :local
 
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -30,6 +29,8 @@ module DesafioProgramacao1b
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"ptBR"
     config.i18n.fallbacks = false
+
+    config.autoload_paths += %W(#{config.root}/importacao/extensions)
 
   end
 end
