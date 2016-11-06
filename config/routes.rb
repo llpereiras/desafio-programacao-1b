940a01fab0654eb0b15d21fece7c499b6b504e5a
Leandro Pereira <llpereiras@gmail.com>
2016-11-06 15:35:50 -0200

    Refazendo rota para receber o parametro do upload

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root "home#index"
   resources :uploads, except: [:delete, :update]
   get "uploads/reprocessar/:id" => "uploads#reprocessar"
+  get "vendas/:upload_id" => "vendas#index"
 
   resources :produtos, only: [:index]
-  resources :vendas, only: [:index]
   resources :enderecos, only: [:index]
   resources :compradores, only: [:index]
   resources :fornecedores, only: [:index]
