b5bd736ef8a6af449bf1a722a9e24252170ac9eb
Leandro Pereira <llpereiras@gmail.com>
2016-11-06 13:36:18 -0200

    Ajustando método de criar venda

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,16 +1,16 @@
 class Venda < ActiveRecord::Base
-  belongs_to :cliente
+  belongs_to :comprador
   belongs_to :fornecedor
   belongs_to :produto
 
   def self.criar(data)
-
-    Venda.
-    # TODO Criar adapter
-    # TODO Verificar tratamentos a fazer
-    # TODO Criar documentacao
-    # TODO crair home
-
-    puts "Criar registros"
+    begin
+      comprador = Comprador.busca_cadastra(data[0], data[4])
+      produto = Produto.busca_cadastra(data[1], data[2], data[3])
+      fornecedor = Fornecedor.busca_cadastra(data[5])
+      Venda.create({comprador: comprador, produto: produto, fornecedor: fornecedor})
+    rescue
+      raise "Impossível criar vendas"
+    end
   end
 end
