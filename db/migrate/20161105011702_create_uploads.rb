66f072dc5a2b9cfbf882e1e8ce44f80168d03d47
Leandro Pereira <llpereiras@gmail.com>
2016-11-06 12:23:16 -0200

    Versionando migrates corretas para uma versão alpha da aplicação.

--- db/migrate/20161105011702_create_uploads.rb
+++ db/migrate/20161105011702_create_uploads.rb
@@ -2,7 +2,7 @@ class CreateUploads < ActiveRecord::Migration
   def change
     create_table :uploads do |t|
       t.string :arquivo
-      t.integer :status, default: 0
+      t.integer :status, default: '0'
 
       t.timestamps null: false
     end
