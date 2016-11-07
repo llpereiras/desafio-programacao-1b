87889c2e18c32e09b2d0e006cb3bdc2fd156cec0
Leandro Machado Pereira <llpereiras@gmail.com>
2016-11-07 17:47:10 -0200

    Refatorando testes dos models

--- spec/models/endereco_spec.rb
+++ spec/models/endereco_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Endereco, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  let(:atributos_validos) { {comprador: FactoryGirl.create(:comprador), logradouro: Faker::Lorem.words(3) }}
+  let(:atributos_nao_validos) { {comprador: ''}}
+
+  describe "Tentar criar endereco" do
+    it " com um nome qualquer" do
+      endereco = Endereco.create(atributos_validos)
+      expect(endereco.save).to be(true)
+    end
+
+    it " sem um nome" do
+      endereco = Endereco.create(atributos_nao_validos)
+      expect(endereco.save).to be(false)
+    end
+  end
+
 end
